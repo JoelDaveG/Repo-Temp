9c18b9b1020024650412ea5efa74041e5d590275
DarckLuck <tableofwork@gmail.com>
2024-04-26 10:16:38 -0500

    Page tempo

---
+++ Models PQR/test.js
@@ -0,0 +1,9 @@
+function solution(value){
+    let type = typeof(value);
+    return type;
+};
+
+console.log(solution(1,'Dieguito', true));
+
+// let list = [1,2,3,4,5,['manzana','pera','banana']];
+// console.log(typeof(list));
